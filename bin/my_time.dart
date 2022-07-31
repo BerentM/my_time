b1da7f70a0fb9f6020135e3e01b0fe9949c708f3
BerentM <berentm@protonmail.com>
2022-07-31 22:20:28 +0200

    refactor: moved to CommandRunner for better readability

--- bin/my_time.dart
+++ bin/my_time.dart
@@ -1,33 +1,5 @@
-import 'dart:io';
-import 'package:args/args.dart';
-import 'package:my_time/my_time.dart' as my_time;
 import 'package:my_time/cli.dart' as cli;
 
 void main(List<String> arguments) {
-  var parser = cli.main();
-  try {
-    var parsed = parser.parse(arguments);
-    print('Hello world: ${my_time.calculate()}!');
-    if ((parsed.command?.name != null) & parsed.wasParsed("help")) {
-      displayHelp(parser.commands[parsed.command!.name]!);
-    } else if (parsed.wasParsed("help")) {
-      displayHelp(parser);
-    } else {
-      stdout.writeln(parsed.command!.name);
-      stdout.writeln(parsed.command!["path"]);
-    }
-  } catch (e) {
-    stderr.writeln(e);
-    stderr.writeln("Please refer --help for further assistance.");
-    exitCode = 2;
-  }
-}
-
-void displayHelp(ArgParser parser) {
-  stdout.writeln(parser.usage);
-  for (var k in parser.commands.keys) {
-    stdout.writeln();
-    stdout.writeln(k);
-    stdout.writeln(parser.commands[k]?.usage);
-  }
+  cli.main(arguments);
 }
--- lib/cli.dart
+++ lib/cli.dart
@@ -1,32 +1,73 @@
 library cli;
 
-import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'dart:io';
 
-ArgParser main() {
-  var today = DateTime.now();
-  var parser = ArgParser();
-  parser.addFlag(
-    "help",
-    abbr: "h",
-    help: "Display help menu.",
-    negatable: false,
-  );
+void main(List<String> args) {
+  var runner = CommandRunner("my_time", """
+== Quick Start ${'=' * 44}
+If you want add task 'today's task'
+my_time.exe add today's task
 
-  var show = parser.addCommand("show");
-  show.addOption(
-    "path",
-    abbr: "p",
-    help: "CSV file path.",
-    defaultsTo: "${"${today.month}".padLeft(2, "0")}${today.year}.csv",
-  );
+If you want to display current month tasks
+my_time.exe show
+${'=' * 59} """);
+  runner.addCommand(AddCommand());
+  runner.addCommand(ShowCommand());
 
-  var add = parser.addCommand("add");
-  add.addOption(
-    "path",
-    abbr: "p",
-    help: "CSV file path.",
-    defaultsTo: "${"${today.month}".padLeft(2, "0")}${today.year}.csv",
-  );
-
-  return parser;
+  runner.run(args).catchError((error) {
+    print(error);
+    exit(64); // Exit code 64 indicates a usage error.
+  });
 }
+
+class AddCommand extends Command {
+  // The [name] and [description] properties must be defined by every
+  // subclass.
+  var today = DateTime.now();
+  @override
+  final name = "add";
+  @override
+  final description = "Add new row to CSV file.";
+
+  AddCommand() {
+    argParser.addOption(
+      "path",
+      abbr: "p",
+      help: "CSV file path.",
+      defaultsTo: "${"${today.month}".padLeft(2, "0")}${today.year}.csv",
+    );
+  }
+
+  @override
+  void run() {
+    print(argResults?.arguments);
+  }
+}
+
+class ShowCommand extends Command {
+  // The [name] and [description] properties must be defined by every
+  // subclass.
+  var today = DateTime.now();
+  @override
+  final name = "show";
+  @override
+  final description = "Show CSV file contents.";
+
+  ShowCommand() {
+    argParser.addOption(
+      "path",
+      abbr: "p",
+      help: "CSV file path.",
+      defaultsTo: "${"${today.month}".padLeft(2, "0")}${today.year}.csv",
+    );
+  }
+
+  @override
+  void run() {
+    print(argResults?.arguments);
+  }
+}
+
+//   runner.argParser.addSeparator("""
+// """);
