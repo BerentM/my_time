522156f46b1dcc32dcaa14046b5834fa9bf203fe
BerentM <berentm@protonmail.com>
2022-08-06 20:41:59 +0200

    refactor: moved show logic from cli.dart to my_time.dart

--- lib/cli.dart
+++ lib/cli.dart
@@ -3,6 +3,8 @@ library cli;
 import 'package:args/command_runner.dart';
 import 'dart:io';
 
+import 'package:my_time/my_time.dart';
+
 void main(List<String> args) {
   var runner = CommandRunner("my_time", """
 == Quick Start ${'=' * 44}
@@ -125,93 +127,9 @@ class ShowCommand extends Command {
 
   @override
   void run() async {
-    // TODO: refactor it, move below spaghetti to separate functions
     var path = argResults?["path"];
     if (path != null) {
-      final contents = await File(path).readAsLines();
-      contents.sort();
-      if (argResults?["time"] == true) {
-        var timeSum = 0.0;
-        for (var line in contents) {
-          timeSum += double.parse(line.split(";")[1]);
-        }
-        if (argResults?["verbose"] == true) {
-          final daysReported = contents.length;
-          final meanTime = timeSum / contents.length;
-          final firstReportedDay = DateTime.parse(contents[0].split(";")[0]);
-          final firstMonthDay = DateTime(
-            firstReportedDay.year,
-            firstReportedDay.month,
-            1,
-          );
-          final lastReportedDay = DateTime.parse(
-            contents[contents.length - 1].split(";")[0],
-          );
-          final lastDayOfMonth = DateTime(
-            lastReportedDay.year,
-            lastReportedDay.month + 1,
-            0,
-          );
-          final possibleDaysCount =
-              lastDayOfMonth.difference(firstMonthDay).inDays + 1;
-          final daysList = List.generate(
-            possibleDaysCount,
-            (i) => DateTime(
-              firstMonthDay.year,
-              firstMonthDay.month,
-              firstMonthDay.day + (i),
-            ),
-          );
-          var workingDays = 0;
-          for (var day in daysList) {
-            if (day.weekday < 6) {
-              workingDays += 1;
-            }
-          }
-
-          stdout.writeln("\tSummary");
-          stdout.writeln(
-            "Total:\t$timeSum ${timeSum < 2 ? 'hour' : 'hours'}",
-          );
-          if (argResults?["earnings"] != null) {
-            // 0.88 because of PPE tax
-            final earnings =
-                (timeSum * double.parse(argResults?["earnings"])) * 0.88;
-            stdout.writeln("Earned:\t${earnings.toStringAsFixed(2)} PLN");
-          }
-          stdout.writeln(
-            "Mean:\t${meanTime.toStringAsFixed(2)} ${meanTime < 2 ? 'hour/day' : 'hours/day'}",
-          );
-          stdout.writeln(
-            "Days:\t$daysReported/$workingDays days",
-          );
-        } else {
-          stdout.writeln(timeSum);
-        }
-      }
-      if (argResults?["all"] == true) {
-        if (argResults?["verbose"] == true) {
-          stdout.writeln("Day\t\tHours\tDescription");
-          for (var line in contents) {
-            var lineList = line.split(";");
-            stdout.writeln(lineList.join("\t"));
-          }
-        } else {
-          for (var line in contents) {
-            stdout.writeln(line);
-          }
-        }
-      }
-      if ((argResults?["verbose"] == false) &
-          (argResults?["earnings"] != null)) {
-        var timeSum = 0.0;
-        for (var line in contents) {
-          timeSum += double.parse(line.split(";")[1]);
-        }
-        stdout.writeln(
-            ((timeSum * double.parse(argResults?["earnings"])) * 0.88)
-                .toStringAsFixed(2));
-      }
+      parseShow(argResults!);
     }
   }
 }
--- lib/my_time.dart
+++ lib/my_time.dart
@@ -1,3 +1,138 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+Future<List<String>> readFile(String path) async {
+  final contents = await File(path).readAsLines();
+  contents.sort();
+  return contents;
+}
+
+void parseAllFlag(ArgResults res, List<String> contents) {
+  if (res["verbose"] == true) {
+    stdout.writeln("Day\t\tHours\tDescription");
+    for (var line in contents) {
+      var lineList = line.split(";");
+      stdout.writeln(lineList.join("\t"));
+    }
+  } else {
+    for (var line in contents) {
+      stdout.writeln(line);
+    }
+  }
+}
+
+String calculateEarnings(double hours, String hourlyWage) {
+  // 0.88 because of PPE tax
+  return ((hours * double.parse(hourlyWage)) * 0.88).toStringAsFixed(2);
+}
+
+void parseNonVerboseEarnings(ArgResults res, List<String> contents) {
+  var timeSum = 0.0;
+  for (var line in contents) {
+    timeSum += double.parse(line.split(";")[1]);
+  }
+  stdout.writeln(calculateEarnings(timeSum, res["earnings"]));
+}
+
+int countWorkingDays(DateTime start, DateTime end) {
+  // generate list of days between first and last day of month
+  final daysList = List.generate(
+    end.difference(start).inDays + 1,
+    (i) => DateTime(
+      start.year,
+      start.month,
+      start.day + (i),
+    ),
+  );
+
+  var workingDays = 0;
+  for (var day in daysList) {
+    if (day.weekday < 6) {
+      workingDays += 1;
+    }
+  }
+
+  return workingDays;
+}
+
+double calculateWorkingHours(List<String> contents) {
+  var timeSum = 0.0;
+  for (var line in contents) {
+    timeSum += double.parse(line.split(";")[1]);
+  }
+  return timeSum;
+}
+
+DateTime firstDayOfMonth(String inputDate) {
+  final firstReportedDay = DateTime.parse(inputDate);
+  final firstDayOfMonth = DateTime(
+    firstReportedDay.year,
+    firstReportedDay.month,
+    1,
+  );
+
+  return firstDayOfMonth;
+}
+
+DateTime lastDayOfMonth(String inputDate) {
+  final lastReportedDay = DateTime.parse(inputDate);
+  final lastDayOfMonth = DateTime(
+    lastReportedDay.year,
+    lastReportedDay.month + 1,
+    0,
+  );
+  return lastDayOfMonth;
+}
+
+void timeOutputGeneration(
+  double timeSum,
+  ArgResults res,
+  List<String> contents,
+  DateTime firstDayOfMonth,
+  DateTime lastDayOfMonth,
+) {
+  stdout.writeln("\tSummary");
+  stdout.writeln(
+    "Total:\t$timeSum ${timeSum < 2 ? 'hour' : 'hours'}",
+  );
+  if (res["earnings"] != null) {
+    stdout
+        .writeln("Earned:\t${calculateEarnings(timeSum, res['earnings'])} PLN");
+  }
+  final meanTime = timeSum / contents.length;
+  stdout.writeln(
+    "Mean:\t${meanTime.toStringAsFixed(2)} ${meanTime < 2 ? 'hour/day' : 'hours/day'}",
+  );
+  stdout.writeln(
+    "Days:\t${contents.length}/${countWorkingDays(firstDayOfMonth, lastDayOfMonth)}days",
+  );
+}
+
+void parseTimeFlag(ArgResults res, List<String> contents) {
+  var timeSum = calculateWorkingHours(contents);
+  if (res["verbose"] == true) {
+    timeOutputGeneration(
+      timeSum,
+      res,
+      contents,
+      firstDayOfMonth(contents[0].split(";")[0]),
+      lastDayOfMonth(contents[contents.length - 1].split(";")[0]),
+    );
+  } else {
+    stdout.writeln(timeSum);
+  }
+}
+
+void parseShow(ArgResults res) async {
+  var contents = await readFile(res["path"]);
+  if (res["time"] == true) {
+    parseTimeFlag(res, contents);
+  }
+  if (res["all"] == true) {
+    parseAllFlag(res, contents);
+  }
+  if ((res["verbose"] == false) & (res["earnings"] != null)) {
+    parseNonVerboseEarnings(res, contents);
+  }
 }
