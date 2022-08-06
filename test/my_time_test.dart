0d196885c38957751047729f6a1fc0456db5fdc4
BerentM <berentm@protonmail.com>
2022-08-06 21:01:44 +0200

    feat: added few basic unit tests

--- test/my_time_test.dart
+++ test/my_time_test.dart
@@ -1,8 +1,29 @@
-// import 'package:my_time/my_time.dart';
-// import 'package:test/test.dart';
+import 'package:my_time/my_time.dart';
+import 'package:test/test.dart';
 
-// void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-// }
+void main() {
+  test('firstDayOfMonth', () {
+    expect(firstDayOfMonth("2022-08-01"), DateTime(2022, 8, 1));
+    expect(firstDayOfMonth("2022-08-06"), DateTime(2022, 8, 1));
+    expect(firstDayOfMonth("2022-08-32"), DateTime(2022, 9, 1));
+  });
+  test('lastDayOfMonth', () {
+    expect(lastDayOfMonth("2022-08-01"), DateTime(2022, 8, 31));
+    expect(lastDayOfMonth("2022-08-06"), DateTime(2022, 8, 31));
+    expect(lastDayOfMonth("2022-08-32"), DateTime(2022, 9, 30));
+  });
+  test('countWorkingDays', () {
+    expect(
+      countWorkingDays(DateTime(2022, 08, 01), DateTime(2022, 08, 31)),
+      23,
+    );
+    expect(
+      countWorkingDays(DateTime(2022, 07, 01), DateTime(2022, 07, 31)),
+      21,
+    );
+    expect(
+      countWorkingDays(DateTime(2022, 07, 01), DateTime(2022, 08, 31)),
+      44,
+    );
+  });
+}
