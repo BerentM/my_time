b19e4f7e7dadd185de731b3fbbf058487de5e63a
BerentM <berentm@protonmail.com>
2022-08-06 16:04:04 +0200

    feat: show command logic implementation

--- lib/cli.dart
+++ lib/cli.dart
@@ -56,6 +56,7 @@ class AddCommand extends Command {
   void run() async {
     var path = argResults?["path"];
     if (path != null) {
+      // TODO: add argument type checks, time needs to be numeric type
       final newRow = [
         argResults?["date"],
         argResults?["time"],
@@ -97,20 +98,120 @@ class ShowCommand extends Command {
       help: "CSV file path.",
       defaultsTo: "${"${today.month}".padLeft(2, "0")}${today.year}.csv",
     );
+    argParser.addOption(
+      "earnings",
+      abbr: "e",
+      help: "Your hourly wage in PLN",
+    );
     argParser.addFlag(
       "time",
       abbr: "t",
       help: "Show time summary.",
+      negatable: false,
     );
     argParser.addFlag(
       "all",
       abbr: "a",
       help: "Show whole file.",
+      negatable: false,
+    );
+    argParser.addFlag(
+      "verbose",
+      abbr: "v",
+      help: "Output more descriptive information.",
+      negatable: false,
     );
   }
 
   @override
-  void run() {
-    print(argResults?.arguments);
+  void run() async {
+    // TODO: refactor it, move below spaghetti to separate functions
+    var path = argResults?["path"];
+    if (path != null) {
+      final contents = await File(path).readAsLines();
+      contents.sort();
+      if (argResults?["time"] == true) {
+        var timeSum = 0.0;
+        for (var line in contents) {
+          timeSum += double.parse(line.split(";")[1]);
+        }
+        if (argResults?["verbose"] == true) {
+          final daysReported = contents.length;
+          final meanTime = timeSum / contents.length;
+          final firstReportedDay = DateTime.parse(contents[0].split(";")[0]);
+          final firstMonthDay = DateTime(
+            firstReportedDay.year,
+            firstReportedDay.month,
+            1,
+          );
+          final lastReportedDay = DateTime.parse(
+            contents[contents.length - 1].split(";")[0],
+          );
+          final lastDayOfMonth = DateTime(
+            lastReportedDay.year,
+            lastReportedDay.month + 1,
+            0,
+          );
+          final possibleDaysCount =
+              lastDayOfMonth.difference(firstMonthDay).inDays + 1;
+          final daysList = List.generate(
+            possibleDaysCount,
+            (i) => DateTime(
+              firstMonthDay.year,
+              firstMonthDay.month,
+              firstMonthDay.day + (i),
+            ),
+          );
+          var workingDays = 0;
+          for (var day in daysList) {
+            if (day.weekday < 6) {
+              workingDays += 1;
+            }
+          }
+
+          stdout.writeln("\tSummary");
+          stdout.writeln(
+            "Total:\t$timeSum ${timeSum < 2 ? 'hour' : 'hours'}",
+          );
+          if (argResults?["earnings"] != null) {
+            // 0.88 because of PPE tax
+            final earnings =
+                (timeSum * double.parse(argResults?["earnings"])) * 0.88;
+            stdout.writeln("Earned:\t${earnings.toStringAsFixed(2)} PLN");
+          }
+          stdout.writeln(
+            "Mean:\t${meanTime.toStringAsFixed(2)} ${meanTime < 2 ? 'hour/day' : 'hours/day'}",
+          );
+          stdout.writeln(
+            "Days:\t$daysReported/$workingDays days",
+          );
+        } else {
+          stdout.writeln(timeSum);
+        }
+      }
+      if (argResults?["all"] == true) {
+        if (argResults?["verbose"] == true) {
+          stdout.writeln("Day\t\tHours\tDescription");
+          for (var line in contents) {
+            var lineList = line.split(";");
+            stdout.writeln(lineList.join("\t"));
+          }
+        } else {
+          for (var line in contents) {
+            stdout.writeln(line);
+          }
+        }
+      }
+      if ((argResults?["verbose"] == false) &
+          (argResults?["earnings"] != null)) {
+        var timeSum = 0.0;
+        for (var line in contents) {
+          timeSum += double.parse(line.split(";")[1]);
+        }
+        stdout.writeln(
+            ((timeSum * double.parse(argResults?["earnings"])) * 0.88)
+                .toStringAsFixed(2));
+      }
+    }
   }
 }
